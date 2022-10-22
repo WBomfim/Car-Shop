6f0fd0fa7e8f394fa7ad5cd15f0022f5415defd8
WBomfim <willianbomfim@hotmail.com>
2022-10-22 13:27:21 -0300

    feat: Criado a classe Car

---
+++ src/models/Car.ts
@@ -0,0 +1,19 @@
+import { model as createModel, Schema } from 'mongoose';
+import MongoModel from './MongoModel';
+import { ICar } from '../interfaces/ICar';
+
+const carSchema = new Schema<ICar>({
+  model: { type: String, length: 3 },
+  year: { type: Number, min: 1900, max: 2022 },
+  color: { type: String, length: 3 },
+  status: { type: Boolean, required: false },
+  buyValue: Number,
+  doorsQty: { type: Number, min: 2, max: 4 },
+  seatsQty: { type: Number, min: 2, max: 7 },
+});
+
+export default class Car extends MongoModel<ICar> {
+  constructor(model = createModel<ICar>('Car', carSchema)) {
+    super(model);
+  }
+}
