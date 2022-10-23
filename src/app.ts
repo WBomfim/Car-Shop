c3ad432b578590d997ea1ad4e8051fcd1736bb2a
WBomfim <willianbomfim@hotmail.com>
2022-10-22 21:51:15 -0300

    feat: Criado o middle handleerrors para realizar gestão dos erros emitidos na aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
+import errorHandler from './middleware/handleErrors';
 import carRoutes from './routes/car';
 
 const app = express();
 
 app.use(express.json());
 app.use('/cars', carRoutes);
+app.use(errorHandler);
 
 export default app;
