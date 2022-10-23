71d97af055390cef3d1ef9ae886dd7b782e2771e
WBomfim <willianbomfim@hotmail.com>
2022-10-22 22:05:50 -0300

    feat: Personalizado as mensagens de retorno dos erros das validação do Zod

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -2,10 +2,16 @@ import { z } from 'zod';
 import { vehicleZodSchema } from './IVehicle';
 
 const carZodSchema = vehicleZodSchema.extend({
-  doorsQty: z.number().int().positive().gte(2)
-    .lte(4),
-  seatsQty: z.number().int().positive().gte(2)
-    .lte(7),
+  doorsQty: z.number({ required_error: 'Doors quantity is required' })
+    .int({ message: 'Doors quantity must be an integer' })
+    .positive({ message: 'Doors quantity must be a positive number' })
+    .gte(2, { message: 'Doors quantity must be greater than or equal to 2' })
+    .lte(4, { message: 'Doors quantity must be less than or equal to 4' }),
+  seatsQty: z.number({ required_error: 'Seats quantity is required' })
+    .int({ message: 'Seats quantity must be an integer' })
+    .positive({ message: 'Seats quantity must be a positive number' })
+    .gte(2, { message: 'Seats quantity must be greater than or equal to 2' })
+    .lte(7, { message: 'Seats quantity must be less than or equal to 7' }),
 });
 
 export type ICar = z.infer<typeof carZodSchema>;
--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,12 +1,19 @@
 import { z } from 'zod';
 
 const vehicleZodSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().int().positive().gte(1900)
-    .lte(2022),
-  color: z.string().min(3),
-  status: z.boolean().optional(),
-  buyValue: z.number().int().positive(),
+  model: z.string({ required_error: 'Model is required' })
+    .min(3, { message: 'Model must be at least 3 characters' }),
+  year: z.number({ required_error: 'Year is required' })
+    .int({ message: 'Year must be an integer' })
+    .positive({ message: 'Year must be a positive number' })
+    .gte(1900, { message: 'Year must be greater than or equal to 1900' })
+    .lte(2022, { message: 'Year must be less than or equal to 2022' }),
+  color: z.string({ required_error: 'Color is required' })
+    .min(3, { message: 'Color must be at least 3 characters' }),
+  status: z.boolean({ invalid_type_error: 'Status must be a boolean' }).optional(),
+  buyValue: z.number({ required_error: 'Buy value is required' })
+    .int({ message: 'Buy value must be an integer' })
+    .positive({ message: 'Buy value must be a positive number' }),
 });
 
 export type IVehicle = z.infer<typeof vehicleZodSchema>;
